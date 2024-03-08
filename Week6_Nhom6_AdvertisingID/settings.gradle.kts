ac4c6480c68da16867f4e0080e86a89d4a19548b
Anh Khoa Mark <qazblp2773652@gmail.com>
2024-03-08 13:26:35 +0700

    first push project

---
+++ Week6_Nhom6_AdvertisingID/settings.gradle.kts
@@ -0,0 +1,18 @@
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "Week6_Nhom6_AdvertisingID"
+include(":app")
+ 
